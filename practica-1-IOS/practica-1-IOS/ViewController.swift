f5dfbb1acd905c47f618a640391efb98deca54f6
XxjesusalbertoxX <villarrealperezjesusalberto@gmail.com>
2026-09-17 17:39:47 -0600

    Inicio de proyecto

---
+++ practica-1-IOS/practica-1-IOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  practica-1-IOS
+//
+//  Created by Jesus Alberto Villarreal Perez on 17/09/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
